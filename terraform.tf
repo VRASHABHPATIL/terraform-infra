e3df34367e71ecdcb81b445dd4dad205578555c3
VRASHABHPATIL <vrashabhpatil047@gmail.com>
2025-06-03 09:15:25 +0100

    updated the backend code in terraform

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,9 @@ terraform {
       version = "5.94.1"
     }
   }
+   backend "s3" {
+    bucket         = "ittsecurestore"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+  }
 }
